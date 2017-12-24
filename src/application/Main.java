a53c2729994f60dbfa7ad5fbcecdc87f173170db
Chataignon Corentin <corentin.chataignon44@gmail.com>
2017-12-24 16:38:28 +0100

    :tada: Initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,61 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+    private BorderPane rootLayout;
+    private Stage primaryStage;
+    
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			this.primaryStage = primaryStage;
+			
+			initRootLayout();
+			showAvionOverview();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void initRootLayout() {
+	    try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/templates/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public void showAvionOverview() {
+	    try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/templates/AvionOverview.fxml"));
+            AnchorPane avionOverview = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(avionOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
